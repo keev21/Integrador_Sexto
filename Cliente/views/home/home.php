9e64e2533a7c4a67db2cbca06919cbcc2ab6cd98
keev21 <kevinsan1835@gmail.com>
2023-12-22 17:40:04 -0500

    menu

--- Cliente/views/home/home.php
+++ Cliente/views/home/home.php
@@ -1,15 +1,15 @@
 
 <div class="col-lg-12">
                     <div class="section-title">
-                        <h2>Featured Product</h2>
+                        <h2>Productos destacados</h2>
                     </div>
                     <div class="featured__controls">
                         <ul>
-                            <li class="active" data-filter="*">All</li>
-                            <li data-filter=".oranges">Oranges</li>
-                            <li data-filter=".fresh-meat">Fresh Meat</li>
-                            <li data-filter=".vegetables">Vegetables</li>
-                            <li data-filter=".fastfood">Fastfood</li>
+                            <li class="active" data-filter="*">Todos</li>
+                            <li data-filter=".fresh-meat">Procesadores</li>
+                            <li data-filter=".oranges">Tarjetas Gráficas</li>
+                            <li data-filter=".vegetables">Almacenamiento</li>
+                            <li data-filter=".fastfood">RAM</li>
                         </ul>
                     </div>
                 </div>
@@ -165,7 +165,7 @@
             <div class="row">
                 <div class="col-lg-4 col-md-6">
                     <div class="latest-product__text">
-                        <h4>Latest Products</h4>
+                        <h4>Últimos productos</h4>
                         <div class="latest-product__slider owl-carousel">
                             <div class="latest-prdouct__slider__item">
                                 <a href="#" class="latest-product__item">
@@ -230,7 +230,7 @@
                 </div>
                 <div class="col-lg-4 col-md-6">
                     <div class="latest-product__text">
-                        <h4>Top Rated Products</h4>
+                        <h4>Productos mejor valorados  </h4>
                         <div class="latest-product__slider owl-carousel">
                             <div class="latest-prdouct__slider__item">
                                 <a href="#" class="latest-product__item">
@@ -295,7 +295,7 @@
                 </div>
                 <div class="col-lg-4 col-md-6">
                     <div class="latest-product__text">
-                        <h4>Review Products</h4>
+                        <h4>Revisión de productos</h4>
                         <div class="latest-product__slider owl-carousel">
                             <div class="latest-prdouct__slider__item">
                                 <a href="#" class="latest-product__item">
--- Cliente/views/html/menu.php
+++ Cliente/views/html/menu.php
@@ -11,8 +11,8 @@
                  <div class="col-lg-6 col-md-6">
                      <div class="header__top__left">
                          <ul>
-                             <li><i class="fa fa-envelope"></i> hello@colorlib.com</li>
-                             <li>Free Shipping for all Order of $99</li>
+                             <li><i class="fa fa-envelope"></i>gamecenter34@gmail.com</li>
+
                          </ul>
                      </div>
                  </div>
@@ -24,15 +24,7 @@
                              <a href="#"><i class="fa fa-linkedin"></i></a>
                              <a href="#"><i class="fa fa-pinterest-p"></i></a>
                          </div>
-                         <div class="header__top__right__language">
-                             <img src="img/language.png" alt="">
-                             <div>English</div>
-                             <span class="arrow_carrot-down"></span>
-                             <ul>
-                                 <li><a href="#">Spanis</a></li>
-                                 <li><a href="#">English</a></li>
-                             </ul>
-                         </div>
+
                          <div class="header__top__right__auth">
                              <a href="#"><i class="fa fa-user"></i> Login</a>
                          </div>
@@ -44,9 +36,25 @@
      <div class="container">
          <div class="row">
              <div class="col-lg-3">
-                 <div class="header__logo">
-                     <a href="./index.html"><img src="../../Public/img/logo.png" alt=""></a>
+                 <style>
+                     .header__logo img {
+                         width: 200px;
+                         /* Puedes ajustar el tamaño según tus necesidades */
+                         margin-top: -70px;
+                         /* Puedes ajustar la posición hacia arriba según tus necesidades */
+                         margin-bottom: -50px;
+                         /* Puedes ajustar la posición hacia abajo según tus necesidades */
+                         margin-left: 30px;
+                         /* Puedes ajustar la posición hacia la izquierda según tus necesidades */
+                         margin-right: -20px;
+                         /* Puedes ajustar la posición hacia la derecha según tus necesidades */
 
+
+                     }
+                 </style>
+
+                 <div class="header__logo">
+                     <a href="./index.php?page=home/home"><img src="../../Admin/Public/assets/images/logos/favicon.png" alt=""></a>
                  </div>
              </div>
              <div class="col-lg-6">
@@ -54,14 +62,7 @@
                      <ul>
                          <li class="active"><a href="./index.php?page=home/home">Home</a></li>
                          <li><a href="./index.php?page=tienda/tienda">Tienda</a></li>
-                         <li><a href="#">Pages</a>
-                             <ul class="header__menu__dropdown">
-                                 <li><a href="./index.php?page=detalles_producto/detalles_producto">detalles producto</a></li>
-                                
-                                 <li><a href="./index.php?page=pago/pago">Pago</a></li>
 
-                             </ul>
-                         </li>
                          <li><a href="./index.php?page=carrito/carrito">Carrito de Compras</a></li>
                          <li><a href="./index.php?page=contacto/contacto">Contacto</a></li>
                      </ul>
@@ -85,19 +86,26 @@
  <!-- Header Section End -->
 
  <!-- Hero Section Begin -->
- <section class="hero">
+ <section class="hero hero-normal">
      <div class="container">
          <div class="row">
              <div class="col-lg-3">
                  <div class="hero__categories">
                      <div class="hero__categories__all">
                          <i class="fa fa-bars"></i>
-                         <span>All departments</span>
+                         <span>Todas las Categorias</span>
                      </div>
                      <ul>
-                         <li><a href="#">Fresh Meat</a></li>
-                         <li><a href="#">Vegetables</a></li>
-
+                         <li><a href="#">Procesadores</a></li>
+                         <li><a href="#">Tarjeta Gráfica</a></li>
+                         <li><a href="#">RAM</a></li>
+                         <li><a href="#">Placa Base</a></li>
+                         <li><a href="#">Almacenamiento (HDD/SSD)</a></li>
+                         <li><a href="#">Unidad de Suministro de Energía (PSU)</a></li>
+                         <li><a href="#">Sistema de Refrigeración (Ventiladores/Disipadores)</a></li>
+                         <li><a href="#">Caja</a></li>
+                         <li><a href="#">Dispositivos Periféricos </a></li>
+                         
                      </ul>
                  </div>
              </div>
@@ -105,12 +113,9 @@
                  <div class="hero__search">
                      <div class="hero__search__form">
                          <form action="#">
-                             <div class="hero__search__categories">
-                                 All Categories
-                                 <span class="arrow_carrot-down"></span>
-                             </div>
-                             <input type="text" placeholder="What do yo u need?">
-                             <button type="submit" class="site-btn">SEARCH</button>
+
+                             <input type="text" placeholder="¿Qué nesesitas?">
+                             <button type="submit" class="site-btn">BUSCAR</button>
                          </form>
                      </div>
                      <div class="hero__search__phone">
@@ -118,12 +123,11 @@
                              <i class="fa fa-phone"></i>
                          </div>
                          <div class="hero__search__phone__text">
-                             <h5>+65 11.188.888</h5>
-                             <span>support 24/7 time</span>
+                             <h5> 099 383 7234</h5>
+                             <span>Soporte</span>
                          </div>
                      </div>
                  </div>
-
              </div>
          </div>
      </div>
