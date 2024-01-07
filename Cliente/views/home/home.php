162706b673d9167e451d5fb559644599a1c50600
keev21 <kevinsan1835@gmail.com>
2024-01-07 16:41:41 -0500

    pagina principal

--- Cliente/views/home/home.php
+++ Cliente/views/home/home.php
@@ -14,7 +14,7 @@
 
 <div class="col-lg-12">
     <div class="section-title">
-        <h2>Productos destacados</h2>
+        <h2>Últimos Productos</h2>
         
     </div>
    
@@ -40,8 +40,8 @@
         $nombre = $fila['Nombre'];
         $precio = $fila['Precio'];
         $imagen = $fila['Imagen'];
-        $anchoImagen = 300;
-        $altoImagen = 200;
+        $anchoImagen = 250;
+        $altoImagen = 150;
 
         // Mostrar la información en el HTML
         echo '<div class="col-lg-3 col-md-4 col-sm-6 mix">
@@ -66,7 +66,7 @@
     mysqli_free_result($resultado);
 
     // Cerrar la conexión
-    mysqli_close($conexion);
+    
 ?>
 
 
@@ -82,219 +82,159 @@
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-6">
                 <div class="banner__pic">
-                    <img src="../Public/img/i7.png" alt="">
-                   
+                    <img src="../Public/img/i7.png" alt="" style="width: 500px;">
                 </div>
             </div>
             <div class="col-lg-6 col-md-6 col-sm-6">
                 <div class="banner__pic">
-                    <img src="../Public/img/3060.png" alt="">
+                    <img src="../Public/img/3060.png" alt="" style="width: 500px;">
+                    <br>
+                    <br>
+                    <br>
                 </div>
             </div>
         </div>
     </div>
 </div>
 <!-- Banner End -->
+<?php
 
+
+// Consulta SQL con INNER JOIN
+$query = "SELECT productos.Nombre AS NombreProducto, productos.Precio, productos.Imagen, categorias.Nombre AS NombreCategoria
+FROM productos
+INNER JOIN categorias ON productos.CategoriaID = categorias.CategoriaID
+WHERE categorias.Nombre = 'Procesadores'
+LIMIT 4"; // Limita la consulta a 4 resultados
+
+$resultado = mysqli_query($conexion, $query);
+
+if (!$resultado) {
+    die("Error en la consulta: " . mysqli_error($conexion));
+}
+
+?>
+
+<div class="col-lg-12">
+    <div class="section-title">
+        <h2>Productos destacados</h2>
+    </div>
+</div>
 <!-- Latest Product Section Begin -->
 <section class="latest-product spad">
     <div class="container">
         <div class="row">
             <div class="col-lg-4 col-md-6">
-                <div class="latest-product__text">
-                    <h4>Últimos productos</h4>
-                    <div class="latest-product__slider owl-carousel">
-                        <div class="latest-prdouct__slider__item">
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-1.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-2.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-3.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="latest-prdouct__slider__item">
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-1.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-2.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-3.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="latest-product__text">
-                    <h4>Productos mejor valorados </h4>
-                    <div class="latest-product__slider owl-carousel">
-                        <div class="latest-prdouct__slider__item">
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-1.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-2.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-3.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="latest-prdouct__slider__item">
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-1.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-2.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-3.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="latest-product__text">
-                    <h4>Revisión de productos</h4>
-                    <div class="latest-product__slider owl-carousel">
-                        <div class="latest-prdouct__slider__item">
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-1.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-2.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-3.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="latest-prdouct__slider__item">
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-1.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-2.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                            <a href="#" class="latest-product__item">
-                                <div class="latest-product__item__pic">
-                                    <img src="img/latest-product/lp-3.jpg" alt="">
-                                </div>
-                                <div class="latest-product__item__text">
-                                    <h6>Crab Pool Security</h6>
-                                    <span>$30.00</span>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div class="latest-product__text">
+        <h4>Procesadores</h4>
+        <div class="latest-product__slider owl-carousel">
+            <?php
+            while ($fila = mysqli_fetch_assoc($resultado)) {
+                echo '<div class="latest-prdouct__slider__item">
+                        <a href="#" class="latest-product__item">
+                            <div class="latest-product__item__pic">
+                                <img src="' . $fila['Imagen'] . '" alt="">
+                            </div>
+                            <div class="latest-product__item__text">
+                                <h6>' . $fila['NombreProducto'] . '</h6>
+                                <span>' . $fila['Precio'] . '</span>
+                            </div>
+                        </a>
+                    </div>';
+            }
+            ?>
+        </div>
+    </div>
+</div>
+<?php
+
+
+// Consulta SQL con INNER JOIN y filtro por categoría "Tarjetas Gráficas"
+$query = "SELECT productos.Nombre AS NombreProducto, productos.Precio, productos.Imagen
+          FROM productos
+          INNER JOIN categorias ON productos.CategoriaID = categorias.CategoriaID
+          WHERE categorias.Nombre = 'Tarjetas Graficas'
+          LIMIT 2"; // Limita la consulta a 2 resultados
+
+$resultado = mysqli_query($conexion, $query);
+
+if (!$resultado) {
+    die("Error en la consulta: " . mysqli_error($conexion));
+}
+
+?>
+
+
+<div class="col-lg-4 col-md-6">
+    <div class="latest-product__text">
+        <h4>Tarjetas Gráficas</h4>
+        <div class="latest-product__slider owl-carousel">
+            <?php
+            while ($fila = mysqli_fetch_assoc($resultado)) {
+                echo '<div class="latest-prdouct__slider__item">
+                        <a href="#" class="latest-product__item">
+                            <div class="latest-product__item__pic">
+                                <img src="' . $fila['Imagen'] . '" alt="">
+                            </div>
+                            <div class="latest-product__item__text">
+                                <h6>' . $fila['NombreProducto'] . '</h6>
+                                <span>' . $fila['Precio'] . '</span>
+                            </div>
+                        </a>
+                    </div>';
+            }
+            ?>
+        </div>
+    </div>
+</div>
+<?php
+
+
+// Consulta SQL con INNER JOIN y filtro por categoría "Almacenamiento"
+$query = "SELECT productos.Nombre AS NombreProducto, productos.Precio, productos.Imagen
+          FROM productos
+          INNER JOIN categorias ON productos.CategoriaID = categorias.CategoriaID
+          WHERE categorias.Nombre = 'Almacenamiento'
+          LIMIT 2"; // Limita la consulta a 2 resultados
+
+$resultado = mysqli_query($conexion, $query);
+
+if (!$resultado) {
+    die("Error en la consulta: " . mysqli_error($conexion));
+}
+
+?>
+<div class="col-lg-4 col-md-6">
+    <div class="latest-product__text">
+        <h4>Almacenamiento</h4>
+        <div class="latest-product__slider owl-carousel">
+            <?php
+            while ($fila = mysqli_fetch_assoc($resultado)) {
+                echo '<div class="latest-prdouct__slider__item">
+                        <a href="#" class="latest-product__item">
+                            <div class="latest-product__item__pic">
+                                <img src="' . $fila['Imagen'] . '" alt="">
+                            </div>
+                            <div class="latest-product__item__text">
+                                <h6>' . $fila['NombreProducto'] . '</h6>
+                                <span>' . $fila['Precio'] . '</span>
+                            </div>
+                        </a>
+                    </div>';
+            }
+            ?>
+        </div>
+    </div>
+</div>
+
+<?php
+// Cierra la conexión a la base de datos
+mysqli_close($conexion);
+?>
+           
+
+
+
         </div>
     </div>
 </section>
