a67775814b5f8163ff035997ec09c9b277e12ed3
keev21 <kevinsan1835@gmail.com>
2024-03-21 16:27:13 -0500

    iva actualizar

--- Admin/Models/cls_productos2.model.php
+++ Admin/Models/cls_productos2.model.php
@@ -90,21 +90,63 @@ public function insertar($CodigoReferencia, $Nombre, $Precio, $Descripcion, $Ima
     }
 }
 // *********************************************************************************
-    public function actualizar($ProductoID, $CodigoReferencia, $Nombre, $Precio, $Descripcion, $Imagen, $CategoriaID, $FechaIngreso, $Stock, $Iva)
-    {
-        try {
-            $con = new Clase_Conectar_Base_Datos();
-            $con = $con->ProcedimientoConectar();
-            // $cadena = "UPDATE `productos` SET `cedula`='$cedula',`nombre`='$nombre',`apellido`='$apellido',`cargo`='$cargo',`salario`='$salario',`fecha_contratacion`='$fecha_contratacion',`imagen`='$imagen' WHERE ProductoID=$ProductoID";
-            $cadena = "UPDATE `productos` SET `CodigoReferencia`='$CodigoReferencia', `Nombre`='$Nombre',`Precio`='$Precio',`Descripcion`='$Descripcion',`Imagen`='$Imagen',`CategoriaID`='$CategoriaID',`FechaIngreso`='$FechaIngreso',`Stock`='$Stock',`Iva`='$Iva'  WHERE `ProductoID`='$ProductoID'";
-            $result = mysqli_query($con, $cadena);
-            return 'ok';
-        } catch (Throwable $th) {
-            return $th->getMessage();
-        } finally {
-            $con->close();
+public function actualizar($ProductoID, $CodigoReferencia, $Nombre, $Precio, $Descripcion, $Imagen, $CategoriaID, $FechaIngreso, $Stock, $Iva)
+{
+    try {
+        $con = new Clase_Conectar_Base_Datos();
+        $con = $con->ProcedimientoConectar();
+        
+        // Consulta para obtener el precio del producto
+        $consulta_precio = "SELECT Precio FROM productos WHERE ProductoID = '$ProductoID'";
+        $resultado_precio = mysqli_query($con, $consulta_precio);
+
+        if (!$resultado_precio) {
+            throw new Exception("Error al obtener el precio del producto");
         }
+
+        $fila_precio = mysqli_fetch_assoc($resultado_precio);
+        //GUARDA EL PRECIO ACTUAL
+        $precio_obtenido = $fila_precio['Precio'];
+
+        // Consulta para obtener el porcentaje de IVA correspondiente al ID proporcionado
+        $consulta_iva = "SELECT porcentaje FROM iva WHERE id_iva = $Iva";
+        $resultado_iva = mysqli_query($con, $consulta_iva);
+
+        if (!$resultado_iva) {
+            throw new Exception("Error al obtener el porcentaje de IVA");
+        }
+
+        $fila_iva = mysqli_fetch_assoc($resultado_iva);
+        //GUARDA EL IVA
+        $porcentajeObtenido = $fila_iva['porcentaje'];
+        if($Precio!=$precio_obtenido){
+            // Cálculo del precio con IVA
+            $Precio_Iva = $Precio * ($porcentajeObtenido / 100);
+            $Precio_Total = $Precio + $Precio_Iva;
+            $cadena = "UPDATE `productos` SET `CodigoReferencia`='$CodigoReferencia', `Nombre`='$Nombre',`Precio`='$Precio_Total',`Descripcion`='$Descripcion',`Imagen`='$Imagen',`CategoriaID`='$CategoriaID',`FechaIngreso`='$FechaIngreso',`Stock`='$Stock',`Iva`='$Iva'  WHERE `ProductoID`='$ProductoID'";
+        }
+        else{
+            $cadena = "UPDATE `productos` SET `CodigoReferencia`='$CodigoReferencia', `Nombre`='$Nombre',`Precio`='$Precio',`Descripcion`='$Descripcion',`Imagen`='$Imagen',`CategoriaID`='$CategoriaID',`FechaIngreso`='$FechaIngreso',`Stock`='$Stock',`Iva`='$Iva'  WHERE `ProductoID`='$ProductoID'";
+        }
+
+        
+
+        
+        
+        $result = mysqli_query($con, $cadena);
+
+        if (!$result) {
+            throw new Exception("Error al actualizar el producto: " . mysqli_error($con));
+        }
+
+        return 'ok';
+    } catch (Throwable $th) {
+        return $th->getMessage();
+    } finally {
+        // Cierra la conexión
+        $con->close();
     }
+}
 // *********************************************************************************
     public function eliminar($ProductoID)
     {
