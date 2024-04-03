ffafd641ded6f8d80812afc92580445c93503d5c
keev21 <kevinsan1835@gmail.com>
2024-04-02 19:50:03 -0500

    reporte

--- Admin/Views/reportes/reportes.php
+++ Admin/Views/reportes/reportes.php
@@ -1,5 +1,21 @@
+
 <?php
-
+require_once('../html/head2.php');
 ?>
-
 <iframe title="PROYECTO2" width="1100" height="804" src="https://app.powerbi.com/view?r=eyJrIjoiNGU4ZDI4ZjctMmE5ZC00YTZjLWFkMGQtNDkyNzAxOGZkZDUzIiwidCI6ImRiNDlmMTcxLTlhZWQtNDQ3Ny1hZmRjLWJjYWIwNjllYjc1YiIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+    <br>
+    <br>
+    <br>
+    
+
+    <h6><strong>Intrucciones para descargar el archivo: </strong></h6>
+    <br>
+    <p>•	De click en el botón Exportar el archivo</p>
+<p>•	En la parte superior haga click en el botón “Exportar”</p>
+<p>•	Seleccione cualquiera de las 3 opciones</p>
+<br>
+
+<button type="button" class="btn btn-primary" onclick="window.open('https://app.powerbi.com/groups/me/reports/ab855d1c-4984-4dba-a5a2-7172e871c8ba/ReportSection?experience=power-bi', '_blank');">Exportar el archivo</button>
+
+
+    <?php require_once('../html/script2.php') ?>
